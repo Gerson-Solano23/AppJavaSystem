a27c8c72102a38bab8c72d954753f6e23be50080
Gerson-Solano <83689574+Gerson-Solano@users.noreply.github.com>
2024-05-14 12:55:26 -0600

    feat: add impot CorsConfig class
    creation the import of cors configuration in the main class

--- backend/myAgenda/src/main/java/com/ApiAgenda/myAgenda/MyAgendaApplication.java
+++ backend/myAgenda/src/main/java/com/ApiAgenda/myAgenda/MyAgendaApplication.java
@@ -2,8 +2,10 @@ package com.ApiAgenda.myAgenda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(CorsConfig.class)
 public class MyAgendaApplication {
 
 	public static void main(String[] args) {
