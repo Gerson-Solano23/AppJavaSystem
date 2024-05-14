1564bf7884ce861896d3c2c1fa5da815368cc84e
Gerson-Solano <83689574+Gerson-Solano@users.noreply.github.com>
2024-05-14 10:43:24 -0600

    feat: add contactService
    creation service to use crud from contact entity

---
+++ backend/myAgenda/src/main/java/com/ApiAgenda/myAgenda/Service/contactService.java
@@ -0,0 +1,41 @@
+package com.ApiAgenda.myAgenda.Service;
+
+import com.ApiAgenda.myAgenda.Entity.Contact;
+import com.ApiAgenda.myAgenda.Repository.contactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class contactService {
+    @Autowired
+    private contactRepository contactRepository;
+
+    public Contact addContact(Contact contact){return contactRepository.save(contact);}
+    public Contact getContact(Integer idContact){return contactRepository.findById(idContact).orElse(null);}
+    public List<Contact> getAllContacts(){return (List<Contact>) contactRepository.findAll();}
+    public Contact updateContact(Contact contact){
+        Contact existContact = contactRepository.findById(contact.getIdContact()).orElse(null);
+        existContact.setName(contact.getName());
+        existContact.setPhone(contact.getPhone());
+        existContact.setEmail(contact.getEmail());
+        existContact.setDateOfBirth(contact.getDateOfBirth());
+        return contactRepository.save(existContact);
+    }
+
+    public boolean deleteContact(Integer id){
+        boolean result = true;
+        try {
+            contactRepository.deleteById(id);
+            if (existContact(id)) result = false;
+        }catch (Exception e) {
+            result = false;
+        }
+        return result;
+    }
+
+    public boolean existContact(Integer id){
+        return contactRepository.existsById(id);
+    }
+}
