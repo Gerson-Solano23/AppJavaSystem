5ac8c6cbb367256ebda35c7077b129c010ae6efc
Gerson-Solano <83689574+Gerson-Solano@users.noreply.github.com>
2024-05-14 12:52:04 -0600

    feat: add ResponseDTO
    ResponseDTO  created for send in the endponts results

---
+++ backend/myAgenda/src/main/java/com/ApiAgenda/myAgenda/Entity/ResponseDTO.java
@@ -0,0 +1,16 @@
+package com.ApiAgenda.myAgenda.Entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ResponseDTO {
+    public String message;
+    public String status;
+}
