da1ba697b9673d8215b835820ff676bef4d011f2
Gerson-Solano <83689574+Gerson-Solano@users.noreply.github.com>
2024-05-13 18:21:03 -0600

    feat: add java project
    creation spring-boot and java project

---
+++ backend/myAgenda/src/main/java/com/ApiAgenda/myAgenda/MyAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.ApiAgenda.myAgenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAgendaApplication.class, args);
+	}
+
+}
---
+++ backend/myAgenda/src/test/java/com/ApiAgenda/myAgenda/MyAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApiAgenda.myAgenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
