35619762afa8549153a12429bfdf604ee098dcff
Gerson-Solano <83689574+Gerson-Solano@users.noreply.github.com>
2024-05-15 11:38:50 -0600

    feat: update angular.json, package.json and contactService
    changes in logic funcions to use the api and update the port to start the web app

--- frontend/AppContactList/src/app/services/contact.service.ts
+++ frontend/AppContactList/src/app/services/contact.service.ts
@@ -8,20 +8,22 @@ export class ContactService {
   private http = inject(HttpClient);
 
   list() {
-    return this.http.get('http://localhost:8080/contact/getAll');
+    return this.http.get('http://localhost:3000/api/contact/getAll');
   }
 
   get(id: number) {
-    return this.http.get(`http://localhost:8080/contact/get?id=${id}`);
+    return this.http.get(`http://localhost:3000/api/contact/get?id=${id}`);
   }
 
   create(contact: any) {
-    return this.http.post('http://localhost:8080/contact/create', contact);
+    return this.http.post('http://localhost:3000/api/contact/create', contact);
   }
   update(contact: any) {
-    return this.http.put('http://localhost:8080/contact/update', contact);
+    return this.http.put('http://localhost:3000/api/contact/update', contact);
   }
   delete(id: number) {
-    return this.http.delete(`http://localhost:8080/contact/delete?id=${id}`);
+    return this.http.delete(
+      `http://localhost:3000/api/contact/delete?id=${id}`
+    );
   }
 }
